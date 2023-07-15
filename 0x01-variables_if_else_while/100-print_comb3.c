00d2449823535f0404ba0bbe18a4a3713e23dafa
Yagazieray <macraydike@icloud.com>
2023-07-14 19:39:34 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,43 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  * Return: 0 (Success)
  */
 int main(void)
 {
-	int num1, num2, num3;
+	int d, p;
 
-	num1 = '0';
-	num2 = '0';
-	num3 = '0';
-
-	while (num1 <= '9')
+	for (d = '0'; d < '9'; d++)
 	{
-		while (num2 <= '9')
-		{
-			num3 = '0';
-			while (num3 <= '9')
-			{
-				if (num1 < num2 && num2 < num3)
-				{
-					putchar(num1);
-					putchar(num2);
-					putchar(num3);
 
-					if (num1 != '7')
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				num3++;
-			}
-			num2++;
-		}
-		num1++;
-		num2 = '0';
+	for (p = d + 1; p <= '9'; p++)
+	{
+	if (p != d)
+	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
-
 	return (0);
 }
