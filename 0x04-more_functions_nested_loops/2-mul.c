7911eda03d6b68c129d4eafe1e08f75342c58cd1
Yagazieray <macraydike@icloud.com>
2023-07-20 15:09:43 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - A function that checks two integers
+ * @a: a - Variable
+ * @b: b - Variable
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
