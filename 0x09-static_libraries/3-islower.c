ababbc956b208a28d521aafccd363193e9adb821
Yagazieray <macraydike@icloud.com>
2023-08-04 19:23:44 -0700

    static lib

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - a function that checks for lowercase character
+ * @c: character to be checked
+ * Return: 1 if c is lowercase, otherwise return 0
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
